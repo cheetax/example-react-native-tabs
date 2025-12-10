91094bfe260133c82c9e171445a895eb31e9ff52
cheetax <dmitriy.grebenev@gmail.com>
2025-12-10 15:08:28 +0300

    10122025

--- App.tsx
+++ App.tsx
@@ -1,26 +1,36 @@
 //import Tabs from './Tabs/Tabs';
 import Tabs from '@cheetax/react-native-tabs';
+//import Skia from '@shopify/react-native-skia';
+import { Canvas, Path, PathDef, PathProps, SkPath, Transforms3d, Skia, usePathInterpolation, AnimatedProp, rotate, useCanvasRef, useCanvasSize, Group, rect, FitBox } from '@shopify/react-native-skia';
+//import { Skia } from '@shopify/react-native-skia/lib/typescript/src/skia/types';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { LayoutChangeEvent, StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import Animated, { createAnimatedComponent, useAnimatedReaction, useAnimatedStyle, useSharedValue, withRepeat, withTiming, interpolate, SharedValue, Extrapolation, Easing, withSequence, WithTimingConfig } from 'react-native-reanimated';
+import LoadingIndicator from './indicators/LoadingIndicator';
+
+
 
 export default function App() {
 
   const onLayout = (event: LayoutChangeEvent) => {
-    const { width, height } = event.nativeEvent?.layout || { width: 0, height: 0 }
-    console.log(`Width: ${width}, Height: ${height}`);
+    //const { width, height } = ref.current;
+    //const x = useCanvasSize()
+    // console.log(x);
   }
   return (
-    <SafeAreaView style={styles.container} >
-        <StatusBar style="auto" />
-        <Text>Example react-native-tabs</Text>
-
-        <Tabs content={['Test1', 'Test2', 'Test3r', 'Test1', 'Test1', 'Test1']}
-          duration={200}
-          mode='primary'
-        />
-    </SafeAreaView>
-
+    <View style={[styles.container, { backgroundColor: 'grey' }]} onLayout={onLayout} >
+      <LoadingIndicator
+        variant='small'
+        active={true}
+        visibleContainer
+        size={{
+          sizeContainer: 240,
+          sizeElement: 200
+        }}
+        //colors={{container: 'red', element: 'green'}}
+      />
+    </View>
   );
 }
 
@@ -28,8 +38,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // height: '100%',
-    backgroundColor: '#fff',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
+    //backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
---
+++ indicators/type.ts
@@ -0,0 +1,42 @@
+
+//import { Value } from 'react-native/types_generated/Libraries/Animated/AnimatedExports';
+//import { ValueXY } from 'react-native/types_generated/Libraries/Animated/AnimatedExports';
+//import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { SkPath } from '@shopify/react-native-skia';
+import { MD3Theme, ThemeBase } from 'react-native-paper';
+import { SharedValue } from 'react-native-reanimated';
+
+export type PathType = {
+  [Name: string]: string;
+};
+
+export type PathLoadingProps = {
+  loading: SharedValue<number>;
+  size: number;
+  arr: number[]
+  interpolatePath: SkPath[]
+  color: string
+};
+export type Variant = 'small' | 'standart';
+
+export type SizeType = {
+  sizeElement: number;
+  sizeContainer: number;
+};
+export type ColorsType = {
+  element: string
+  container: string
+}
+export type LoadingIndicatorProps = {
+  variant: Variant;
+  size?: SizeType;
+  active: boolean;
+  visibleContainer?: boolean
+  theme?: MD3Theme
+  colors?: ColorsType
+};
+export type SizeVariantType = {
+  [Name in Variant]: SizeType;
+};
+
